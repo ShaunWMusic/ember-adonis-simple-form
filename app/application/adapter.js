99da3b48af3144ff938032c6da9fad4f0e73efb0
shaunwmusic <shaunwmusic@gmail.com>
2018-02-13 16:37:03 -0600

    adding host namespace to adapter

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -2,7 +2,6 @@ import DS from 'ember-data';
 import config from '../config/environment';
 
 export default DS.JSONAPIAdapter.extend({
-  // authorizer: 'authorizer:token',
 
   host: config.DS.host,
   namespace: config.DS.namespace,
